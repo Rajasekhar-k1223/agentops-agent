8f2beb482a53fe7f699d0858f211eb94d1e22ee5
Rajasekhar.k1223 <rajasekhar.k1223@gmail.com>
2025-06-25 23:24:55 +0530

    25-06-2025_commit

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import platform
 import uuid
 
-SERVER_URL = "http://127.0.0.1:8000"
+SERVER_URL = "https://agentops-server-production.up.railway.app/"
 AUTH_TOKEN = "mysecrettoken"
 
 AGENT_ID = f"{platform.node()}-{uuid.uuid4().hex[:6]}"
